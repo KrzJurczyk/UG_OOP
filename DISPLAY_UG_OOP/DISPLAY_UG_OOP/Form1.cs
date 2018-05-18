ff4a5f380fc467fab60939c5795f2591e158cb1d
DESKTOP-USAOD01\KrisJ <krzjurczyk@gmail.com>
2018-05-18 20:29:48 +0200

    added display and features

---
+++ DISPLAY_UG_OOP/DISPLAY_UG_OOP/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using UG_Project;
+
+namespace DISPLAY_UG_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var a = PicturesProvider.Instance;
+            Image image = a.SearchNewPicture("cat");
+
+            this.pictureBox1.Image = image;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
